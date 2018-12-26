5e3ba5abdfd4e2a0272401cdf21c9b61cf2be8d6
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2018-12-26 18:36:51 +0100

    Fetch result and create regex

--- day-4/src/main.rs
+++ day-4/src/main.rs
@@ -1,9 +1,12 @@
-use std::path::Path;
 use std::fs;
+use std::path::Path;
+
+use regex::Regex;
 
 fn main() {
     let filename = Path::new("./dat/input.dat");
-    let contents = fs::read_to_string(filename);
+    let contents = fs::read_to_string(filename).expect("YOLO");
+    let re_date = Regex::new(r"\d{4}-\d{2}-\d{2}").unwrap();
 
-    println!("{:?}", contents);
+    println!("{}", contents);
 }

2796ae0b77c7e0e3ab2af201943e60732b136f51
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2018-12-28 21:51:29 +0100

    Add reading of file and Lock

--- day-5/src/main.rs
+++ day-5/src/main.rs
@@ -1,3 +1,6 @@
+use std::{fs, path::Path};
+
 fn main() {
-    println!("Hello, world!");
+    let filename = Path::new("./dat/input.dat");
+    let contents: String = fs::read_to_string(filename).unwrap();
 }

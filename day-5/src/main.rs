db8c863c309b23552b9c2b9f1c3cd3045833c261
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-01-02 10:09:16 +0100

    Add solution to day 5 part 1

--- day-5/src/main.rs
+++ day-5/src/main.rs
@@ -1,6 +1,36 @@
+use num::abs;
 use std::{fs, path::Path};
+
+fn remove_pairs(seq: &mut Vec<u8>) {
+    for i in (0..seq.len() - 1).rev() {
+        if i >= seq.len() - 1 {
+            continue;
+        }
+
+        if abs(seq[i + 1] as i8 - seq[i] as i8) == 0x20 {
+            let a = seq.remove(i + 1) as char;
+            let b = seq.remove(i) as char;
+            assert_eq!(a.to_lowercase().to_string(), b.to_lowercase().to_string());
+        }
+    }
+}
 
 fn main() {
     let filename = Path::new("./dat/input.dat");
-    let contents: String = fs::read_to_string(filename).unwrap();
+    let contents = fs::read_to_string(filename).unwrap().trim().to_string();
+
+    let mut seq = contents.into_bytes();
+    let mut prev_len = seq.len();
+
+    loop {
+        remove_pairs(&mut seq);
+
+        if prev_len == seq.len() {
+            break;
+        }
+
+        prev_len = seq.len();
+    }
+
+    println!("Number of units: {}", seq.len());
 }

14fd3e69e322efba56303dacbea5025162e006e2
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-01-02 11:01:47 +0100

    Start on part 1

--- day-6/src/main.rs
+++ day-6/src/main.rs
@@ -1,3 +1,20 @@
+use std::{fs, path::Path};
+use ndarray::prelude::Array;
+
+fn generate_grid(tuples: &str, size: u32) -> Array {
+    let mut grid = Array::zeros(());
+}
+
 fn main() {
-    println!("Hello, world!");
+    let filename = Path::new("./dat/input.dat");
+    let contents = fs::read_to_string(filename).unwrap().trim().to_string();
+
+    let mut num_points = 0;
+
+    for line in contents.lines() {
+        num_points += 1;
+        println!("{}", line);
+    }
+
+    println!("{}", num_points);
 }
